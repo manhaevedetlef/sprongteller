d3bc24cc8f626d118e3eaae80ba80f6cd440e206
manhaevedetlef <58568927+manhaevedetlef@users.noreply.github.com>
2023-03-24 16:20:43 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+//variablen
+let aantalsprongen = 0
+let versnelling =0
+let aantalSprongen=0
+
+//functie
+input.onButtonPressed(Button.A,()=>{
+    aantalsprongen=0
+    basic.showString("go")
+})
+input.onButtonPressed(Button.B,()=>{
+    basic.showString("aantal")
+    basic.showNumber(aantalSprongen)
+})
+
+
+
+basic.forever(function () {
+	
+})
